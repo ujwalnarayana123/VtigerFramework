a0289c33675eed6924fc1720e51ba7a6debdc88a
ujwalnarayana123 <111624915+ujwalnarayana123@users.noreply.github.com>
2022-09-12 18:46:43 +0530

    commit & push

---
+++ src/main/java/Practice/Sampile_Test.java
@@ -0,0 +1,10 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sampile_Test {
+	@Test
+	public void sample() {
+		System.out.println(" sudhr");
+	}
+}
---
+++ src/main/java/Practice/practicetest.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class practicetest {
+
+	@Test
+	public void launch() {
+		System.out.println("my name is ujwal");
+	}
+
+}
