b02bee378df9577f5f60a50746047c85a458a8cb
ujwalnarayana123 <111624915+ujwalnarayana123@users.noreply.github.com>
2022-09-17 14:15:00 +0530

    user 4 commit

--- src/main/java/Practice/practicetest.java
+++ src/main/java/Practice/practicetest.java
@@ -16,7 +16,13 @@ public class practicetest {
 	@Test
 	public void exit() {
 		System.out.println("exit the work space");
-			
+	}
+
+		@Test
+		public void cleardata()
+		{
+			System.out.println("cleardata");
+		
 	}
 
 }
