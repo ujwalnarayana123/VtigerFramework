8929b155d2035c7d7911eaec3d9e6584786b5b6c
ujwalnarayana123 <111624915+ujwalnarayana123@users.noreply.github.com>
2022-09-17 14:31:17 +0530

    6 commit

--- src/main/java/Practice/practicetest.java
+++ src/main/java/Practice/practicetest.java
@@ -7,22 +7,24 @@ public class practicetest {
 	@Test
 	public void launch() {
 		System.out.println("my name is ujwal");
+		System.out.println("my name is manoj");
+
 	}
+
 	@Test
-	public void enterdata()
-	{
+	public void enterdata() {
 		System.out.println("enterdata");
 	}
+
 	@Test
 	public void exit() {
 		System.out.println("exit the work space");
 	}
 
-		@Test
-		public void cleardata()
-		{
-			System.out.println("cleardata");
-		
+	@Test
+	public void cleardata() {
+		System.out.println("cleardata");
+
 	}
 
 }
